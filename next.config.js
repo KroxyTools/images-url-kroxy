147be79b4029bd6b6d45002151ee62b3c7125a76
KroxyTools <kroxytools@gmail.com>
2026-01-21 23:08:47 +0600

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,5 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: { domains: ["cdn.kroxyio.com"] },
+};
+module.exports = nextConfig;
---
+++ siteMeta.js
@@ -0,0 +1,7 @@
+const siteMeta = {
+  title: "Kroxy Image CDN",
+  description: "Fast & easy image CDN for direct and embed links",
+  favicon: "/favicon.ico",
+  domain: "https://cdn.kroxyio.com",
+};
+export default siteMeta;
